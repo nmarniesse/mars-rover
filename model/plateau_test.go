2002253647c64c503c0b72dc8229a343a03c17f3
Nicolas Marniesse <nicolas.marniesse@akeneo.com>
2025-03-29 15:27:51 +0100

    Add test for plateau

---
+++ model/plateau_test.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestItCreatesAPlateau(t *testing.T) {
+	p, err := CreatePlateau(4, 5)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 4, p.MaxX)
+	assert.Equal(t, 5, p.MaxY)
+}
